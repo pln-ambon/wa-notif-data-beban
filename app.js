6ea7f4a33a6167787a0d37db4c5397e0d24ffd63
Arif Santoso <54832098+ariftoteles@users.noreply.github.com>
2025-07-12 19:59:22 +0700

    switch on scheduler data event

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 const { PORT } = require('./config')
 
 const schedulerPowerData = require('./service/scheduler-power-data.service')
-// const schedulerDataEvent = require('./service/scheduler-data-event.service')
+const schedulerDataEvent = require('./service/scheduler-data-event.service')
 
 schedulerPowerData.start()
-// schedulerDataEvent.start()
+schedulerDataEvent.start()
 
 app.listen(PORT, () => {
   console.log("App is running on port " + PORT);
